7d9b51f02a20333ab35b4590f10be4ed0cbad5a2
romario1990 <romario.getulio@gmail.com>
2020-01-17 20:56:46 -0300

    implementado aula 1 aplicação backend

---
+++ backend/src/controllers/SearchController.js
@@ -0,0 +1,38 @@
+const Dev = require('../models/Dev');
+const parseStringAsArray = require('../utils/parseStringAsArray')
+
+module.exports = {
+    async index(request, response){
+        const { latitude, longitude, techs  } = request.query;
+
+        const techsArray = parseStringAsArray(techs);
+
+        console.log(techsArray);
+        const devs = await Dev.find({
+            techs: {
+                $in: techsArray,
+            },
+            location: {
+                $near: {
+                    $geometry: {
+                        type: 'Point',
+                        coordinates: [longitude, latitude],
+                    },
+                    $maxDistance: 10000,
+                },
+            },
+        });
+        return response.json({ devs });
+    },
+
+    async update() {
+        // atualizar name, coordenadas avatar, bio, techs
+        //TO_DO
+    },
+
+    async destroy() {
+        // deletar dev
+        //TO_DO
+    },
+
+};
