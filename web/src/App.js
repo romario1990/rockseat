8f952d0bff4e1eeaa5fc5dbbaf6c1e110a2faddc
romario1990 <romario.getulio@gmail.com>
2020-01-17 21:13:40 -0300

    Conceitos basicos de react

---
+++ web/src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import Header from './Header';
+
+
+
+function App() {
+  const [counter, setCounter] = useState(0);
+  function IncrementCounter(){
+    setCounter(counter + 1);
+  }
+
+  return (
+    <>
+      <h1>Contador: {counter}</h1>
+      <button onClick={IncrementCounter}>Incrementar</button>
+    </>
+  );
+}
+
+export default App;
